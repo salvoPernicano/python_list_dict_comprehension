8669637de283902a62e7c068a1989a33f3e769c1
salvoPernicano <salvo.pernicano@gmail.com>
2025-01-11 14:44:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+nums = [1,2,3,4,5]
+print(nums)
+new_list = [item + 1 for item in nums]
+print(new_list)
+
+name = "Salvatore"
+letters = [letter for letter in name]
+print(letters)
+
+with open("file1.txt") as file:
+        first_file = [int(line.strip()) for line in file.readlines()]
+    
+with open("file2.txt") as file:
+        second_file = [int(line.strip()) for line in file.readlines()]
+
+result = [num for num in first_file if num in second_file]
+
+print(result)
